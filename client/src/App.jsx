f231a7253bcec2a6cba8c13f1829a749f6e96d29
Aditya <adityarajsrvofficial@gmail.com>
2025-09-16 03:11:35 +0530

    Trying to fix production bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import StartSession from "./pages/startSession";
 import Dashboard from "./pages/Dashboard";
 
